2616aa2271eb6a858d73e51ac6c54373a68b9583
ankur841 <coolankur.k@gmail.com>
2015-06-29 23:07:48 +0530

    guage added

--- WebContent/app/components/directive.js
+++ WebContent/app/components/directive.js
@@ -2,26 +2,6 @@
 
 angular.module('investmentApp.common.directive', [])
 
-.directive('mySlider', [function() {
-    return {
-        scope: {
-
-        },
-        compile: function(elem, attrs) {
-            console.log(attrs);
-
-            var s2 = $(elem).freshslider({
-                step: 1,
-                value: 10
-            });
-
-            return function(scope, ele, atts) {
-
-            }
-        }
-    }
-}])
-
 .directive('sliderWithImage', [function() {
     return {
         templateUrl: 'partials/slider-with-image.html',
